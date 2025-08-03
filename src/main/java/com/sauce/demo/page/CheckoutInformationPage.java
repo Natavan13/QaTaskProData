fc8de591d52595c509b0d4dcb2b411ae21a93855
Natavan Pashayeva <natevanpashazade@gmail.com>
2025-08-03 22:22:33 +0400

    some methods regarding filling inout fields are added

--- src/main/java/com/sauce/demo/page/CheckoutInformationPage.java
+++ src/main/java/com/sauce/demo/page/CheckoutInformationPage.java
@@ -7,10 +7,29 @@ public class CheckoutInformationPage extends BasePage {
     By last_name_field = By.id("last-name");
     By postal_code_field = By.id("postal-code");
     By contninue_btn = By.className("btn_primary");
+    By error_msg = By.cssSelector("checkout_info_wrapper h3");
 
     public FinishPage clickContinueBtn(){
         click(contninue_btn);
         return new FinishPage();
+    }
+
+    public void fillFirstName(String firstName){
+        setText(first_name_field, firstName);
+    }
+
+    public void fillLastName(String lastName){
+        setText(last_name_field, lastName);
+    }
+
+    public void fillPostalCode(String postalCode){
+        setText(postal_code_field, postalCode);
+    }
+
+    public void getErrorMsg(){
+        fillFirstName("ewfd");
+        fillLastName("wfd");
+        clickContinueBtn();
     }
 
 
